27de19a4973edc81d43e64535cc31defb3cc2d6a
dolusantecedens <aleksander.marciniak@gmail.com>
2023-02-28 18:12:07 +0100

    s

--- netcat.py
+++ netcat.py
@@ -13,3 +13,21 @@ def execute(cmd):
     output=subprocess.check_output(shlex.split(cmd),
                                     strerr=subprocess.STDOUT)
     return output.decode() 
+if __name__=='__main__':
+    parser = agrprase.ArgumentParser(
+        description='bhp tool',
+        formatter_class=argprase.RawDescriptionHelpFormatter,
+        epilog=textwrap.decent('''example:
+        netcat.py -t 192.168.1.108 -p 5555 -l -c # powloka systemu)
+        netcat.py -t 192.168.1.108 -p 5555 -l -u-mytest.whatisup
+        netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\"
+
+        echo 'ABCDEFGHI' | ./netcat.py -t 192.168.1.108 -p 135 
+
+        netcat.py -y 192.168.1.108 -p 5555 
+        '''))
+    parser.add_argument('-c' '--comand', action='store_true',help='otwarcie powloki')
+    parser.add_argument()
+    parser.add_argument
+    parser.add_argument
+    parser.add_argument
